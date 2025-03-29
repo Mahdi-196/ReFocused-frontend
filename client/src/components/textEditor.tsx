5b866419245bea39f8de99e677d5df0fcd66e82f
Mahdi Ghaleb <mahdighaleb196@gmail.com>
2025-03-29 15:22:09 -0700

    editor working using quill as well as overall clean up

--- client/src/components/textEditor.tsx
+++ client/src/components/textEditor.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
 
-const textEditor = () => {
+const TextEditor = () => {
+  const editorRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (editorRef.current && !editorRef.current.querySelector('.ql-editor')) {
+      new Quill(editorRef.current, {
+        theme: 'snow',
+        modules: {
+          toolbar: [
+            [{ header: [1, 2, 3, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'code'],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            [{ align: [] }],
+            ['blockquote', 'code-block'],
+            ['clean'],
+          ],
+        },
+      });
+    }
+  }, []);
+
   return (
-    <div>textEditor</div>
-  )
-}
+    <div className="max-w-2xl mx-auto mt-10 p-4 border rounded">
+      <div ref={editorRef} className="bg-white min-h-[300px]" />
+    </div>
+  );
+};
 
-export default textEditor
+export default TextEditor;
